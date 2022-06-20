fa61d34bfae68e53b7921997133db605865b9b79
Snezhana <snezhana.bilonoga16@gmail.com>
2022-06-20 18:45:50 +0300

    migrate Sequelize to TypeOrm

--- homework7/src/app.module.ts
+++ homework7/src/app.module.ts
@@ -1,29 +1,26 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { User } from './users/users.model';
 import { Role } from './roles/roles.model';
 import { RolesModule } from './roles/roles.module';
-import { UserRoles } from './roles/user-roles-model';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const envFilePath: string = '.env';
 
 @Module({
-    controllers: [],
-    providers: [],
     imports: [
-        ConfigModule.forRoot({
-            envFilePath: '.env',
-        }),
-        SequelizeModule.forRoot({
-            dialect: 'postgres',
+        ConfigModule.forRoot({ envFilePath }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
             host: process.env.POSTGRES_HOST,
             port: Number(process.env.POSTGRES_PORT),
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [User, Role, UserRoles],
-            autoLoadModels: true,
+            entities: [User, Role],
+            synchronize: true,
         }),
         UsersModule,
         RolesModule,
--- homework7/src/users/users.module.ts
+++ homework7/src/users/users.module.ts
@@ -1,18 +1,17 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './users.model';
 import { Role } from '../roles/roles.model';
-import { UserRoles } from '../roles/user-roles-model';
 import { RolesModule } from '../roles/roles.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     controllers: [UsersController],
     providers: [UsersService],
     imports: [
-        SequelizeModule.forFeature([User, Role, UserRoles]),
+        TypeOrmModule.forFeature([User, Role]),
         RolesModule,
         forwardRef(() => AuthModule),
     ],
